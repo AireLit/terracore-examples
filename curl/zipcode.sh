64451258831eb89413a8bd3a6cc7eb89e7247820
Introvertosaurus <bracken@tnl5.com>
2026-03-08 12:39:25 +0700

    Add script to fetch data by zipcode using API

--- curl/zipcode.sh
+++ curl/zipcode.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+API_KEY="YOUR_API_KEY"
+
+curl -X POST https://api.airelit.com/v1/terracore/zipcode \
+  -H "X-API-Key: ${API_KEY}" \
+  -H "Content-Type: application/json" \
+  -d '{"zipcode":"75201","fields":["summary","housing","income"]}'
